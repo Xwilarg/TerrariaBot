172b140e331714e71719a04971e49e2b388bed2f
Xwilarg <xwilarg@yahoo.fr>
2020-04-11 03:54:58 +0200

    Send message to server

--- TerrariaBot/TerrariaClient.cs
+++ TerrariaBot/TerrariaClient.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Net.Sockets;
+using System.Text;
 using System.Threading;
 
 namespace TerrariaBot
@@ -9,10 +10,13 @@ namespace TerrariaBot
     {
         public TerrariaClient(string ip)
         {
+            _slot = 0;
+
             _client = new TcpClient(ip, 7777);
             _ns = _client.GetStream();
             _listenThread = new Thread(new ThreadStart(Listen));
-            SendMessage(1, version);
+            _listenThread.Start();
+            SendStringMessage(1, version);
         }
 
         ~TerrariaClient()
@@ -24,31 +28,43 @@ namespace TerrariaBot
         {
             while (Thread.CurrentThread.IsAlive)
             {
-                if (_ns.DataAvailable)
+                byte[] buf = new byte[2]; // contains length (uint16)
+                _ns.Read(buf, 0, buf.Length);
+                ushort length = BitConverter.ToUInt16(buf);
+                buf = new byte[1]; // contains type (uint8)
+                _ns.Read(buf, 0, buf.Length);
+                byte type = buf[0];
+                switch (type)
                 {
-                    lock (_ns)
-                    {
-                        BinaryReader reader = new BinaryReader(_ns);
-                        NetworkMessage result = new NetworkMessage();
-                        result.length = reader.ReadUInt16();
-                        result.type = reader.ReadByte();
-                        result.payload = reader.ReadString();
-                        Console.WriteLine("Message received: " + result.type);
-                    }
+                    case 3: // Authentification confirmation
+                        buf = new byte[1];
+                        _ns.Read(buf, 0, buf.Length);
+                        _slot = buf[0];
+                        Console.WriteLine("Player slot is now " + _slot);
+                        break;
+
+                    default:
+                        Console.WriteLine("Unknown message type " + type);
+                        break;
                 }
             }
         }
 
-        public void SendMessage(byte type, string payload)
+        public void SendStringMessage(byte type, string payload)
         {
-            lock (_ns)
-            {
-                BinaryWriter writer = new BinaryWriter(_ns);
-                writer.Write((ushort)payload.Length);
-                writer.Write(type);
-                writer.Write(payload);
-            }
+            var writer = SendMessage((ushort)payload.Length, type);
+            writer.Write(payload);
         }
+
+        private BinaryWriter SendMessage(ushort length, byte type)
+        {
+            BinaryWriter writer = new BinaryWriter(_ns);
+            writer.Write(length);
+            writer.Write(type);
+            return writer;
+        }
+
+        private byte _slot;
 
         private TcpClient _client;
         private NetworkStream _ns;
