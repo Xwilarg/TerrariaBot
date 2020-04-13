f65f6389f10a60f28fbbef22916ee3e34f38e83b
Xwilarg <xwilarg@yahoo.fr>
2020-04-13 14:49:09 +0200

    Add Steam client

---
+++ TerrariaBot.Steam/SteamClient.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace TerrariaBot.Steam
+{
+    public class SteamClient : TerrariaClient
+    {
+        public SteamClient(LogLevel logLevel) : base(logLevel)
+        { }
+    }
+}
